9b40bbe6b548221732a5382d0c629823623d1f78
Konstantin Alekseev <alekseeffkv@gmail.com>
2022-12-12 17:31:11 +0300

    Add Product

---
+++ src/components/Product.tsx
@@ -0,0 +1,66 @@
+import RemoveRoundedIcon from '@mui/icons-material/RemoveRounded';
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  CardContent,
+  CardMedia,
+  IconButton,
+  Typography,
+} from '@mui/material';
+
+type ProductProps = {
+  title: string;
+  brand: string;
+  price: string;
+  image: string;
+};
+
+const Product = ({ title, brand, price, image }: ProductProps) => (
+  <Card component="article">
+    <CardMedia
+      component="img"
+      src={image}
+      alt={title}
+      sx={{ borderRadius: 1 }}
+    />
+
+    <CardContent sx={{ '& > * + *': { mt: '0.5rem!important' } }}>
+      <Typography variant="h5" component="div">
+        {title}
+      </Typography>
+
+      <Typography variant="body2" color="text.secondary">
+        {brand}
+      </Typography>
+
+      <Typography variant="h6" component="div">
+        {price}
+      </Typography>
+
+      <ButtonGroup
+        fullWidth
+        disableElevation
+        sx={{ justifyContent: 'space-between', alignItems: 'center' }}
+      >
+        <IconButton>
+          <RemoveRoundedIcon />
+        </IconButton>
+
+        <Box typography="h6">1 шт</Box>
+
+        <IconButton>
+          <AddRoundedIcon />
+        </IconButton>
+      </ButtonGroup>
+
+      <Button fullWidth disableElevation variant="contained">
+        В корзину
+      </Button>
+    </CardContent>
+  </Card>
+);
+
+export default Product;
